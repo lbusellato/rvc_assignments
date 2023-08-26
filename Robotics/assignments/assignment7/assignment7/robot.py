f62325c6bfea3876bb7d7f17b25ec2c4be8c7066
lbusellato <lorenzo.busellato@gmail.com>
2023-08-26 15:56:16 +0200

    Completed scanning trajectory

--- Robotics/assignments/assignment7/assignment7/robot.py
+++ Robotics/assignments/assignment7/assignment7/robot.py
@@ -1,47 +1,44 @@
+import copy
 import numpy as np
-from rclpy.node import Node
 
-from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
+from rclpy.node import Node
+from geometry_msgs.msg import Pose, Point, Quaternion, PoseStamped
 from sensor_msgs.msg import JointState
-from std_msgs.msg import Float32, Bool
+from std_msgs.msg import Bool
 
 from .trajectory import Trajectory
-
-# Utility variables
-
-RED     = 0
-GREEN   = 1
-BLUE    = 2
-YELLOW  = 3
-CUBE_NAMES =  {RED    : 'red',
-               GREEN  : 'green',
-               BLUE   : 'blu',
-               YELLOW : 'yellow'}
-
-# Some known waypoints, in both operational and joint space
-
-HOME_OP = PoseStamped(pose=Pose(position=Point(x=0.484, y=0.128, z=0.609), orientation=Quaternion(x=0.5, y=0.5, z=0.5, w=0.5)))
-HOME_JOINT = np.array([0.0, -np.pi/2, np.pi/2, np.pi, -np.pi/2, -np.pi/2])
-
-scan_orientation = Quaternion(x=0.0017937315462992396, y=0.9999976506446824, z=0.00035651772527578343, w=-0.001163669784592465)
-SCAN_START_OP = PoseStamped(pose=Pose(position=Point(x=-0.129, y=0.487, z=0.422), 
-                                      orientation=scan_orientation))
-SCAN_START_JOINT = np.array([np.pi/2, -np.pi/2, np.pi/2, -np.pi/2, -np.pi/2, -np.pi/2])
 
 class Robot(Node):
 
     def __init__(self):
         super().__init__('robot')
 
-        self.cube_locations = {RED    : None,
-                               GREEN  : None,
-                               BLUE   : None,
-                               YELLOW : None}
+        # Utility variables
+
+        self.RED     = 0
+        self.GREEN   = 1
+        self.BLUE    = 2
+        self.YELLOW  = 3
+        self.CUBE_NAMES =  {self.RED    : 'red',
+                            self.GREEN  : 'green',
+                            self.BLUE   : 'blu',
+                            self.YELLOW : 'yellow'}
+
+        # Some known waypoints
+
+        self.HOME_OP = PoseStamped(pose=Pose(position=Point(x=0.484, y=0.128, z=0.609), orientation=Quaternion(x=0.5, y=0.5, z=0.5, w=0.5)))
+        self.SCAN_START_OP = PoseStamped(pose=Pose(position=Point(x=-0.129, y=0.487, z=0.422), orientation=Quaternion(x=0.00179, y=0.99999, z=0.00035, w=-0.00116)))
+
+        self.cube_locations = {self.RED    : None,
+                               self.GREEN  : None,
+                               self.BLUE   : None,
+                               self.YELLOW : None}
         
-        self.cube_destinations = {RED    : None,
-                                  GREEN  : None,
-                                  BLUE   : None,
-                                  YELLOW : None}
+        self.cube_destinations = {self.RED    : None,
+                                  self.GREEN  : None,
+                                  self.BLUE   : None,
+                                  self.YELLOW : None}
+        
         # Create the publishers to the target publishing topics
         self.op_target_pub_ = self.create_publisher(PoseStamped, '/ur5/ee_target/pose', 1)
         # Create the publisher for the gripper control topic
@@ -51,7 +48,7 @@ class Robot(Node):
         self.gripper_sub_ = self.create_subscription(JointState, '/joint_states', self.gripper_state_update, 1)
         # Create the subscribers for the cube locations and destinations topics
         self.cube_subs_ = []
-        for key, val in CUBE_NAMES.items():
+        for key, val in self.CUBE_NAMES.items():
             sub = self.create_subscription(PoseStamped, '/cube_' + val + '/pose', self.cube_callback(key, self.cube_locations), 1)
             self.cube_subs_.append(sub)
             sub = self.create_subscription(PoseStamped, '/dest_cube_' + val + '/pose', self.cube_callback(key, self.cube_destinations, 'destination'), 10)
@@ -61,38 +58,29 @@ class Robot(Node):
         self.joint_state = None
         # Gripper state: False for open, True for closed
         self.gripper_state = False
-        # Trajectory planner
-        self.planner = Trajectory()
-        # Generate the scanning trajectory
-        self.scanning_traj = self.generate_scanning_task()
         # Set up the control loop
-        self.control_frequency = 100 #Hz
+        self.control_frequency = 50 #Hz
         self.dt = 1 / self.control_frequency
         self.time = 0
         self.create_timer(timer_period_sec=self.dt, callback=self.control_loop)
+        # Trajectory planner
+        self.planner = Trajectory(dt=self.dt)
+        # Generate the scanning trajectory
+        self.scanning_traj = self.generate_scanning_task()
         # State variables
         self.homed = False
         self.reached_scan_start = False
         self.scanning_done = False
 
     def control_loop(self):
-        if not self.homed:
-            # Go home if not already there
-            self.homed = self.moveL(self.pose, HOME_OP.pose, self.time)
-            if self.homed: self.time = 0
-        elif not self.reached_scan_start:
-            # Go to the starting scanning position
-            self.reached_scan_start = self.moveL(self.pose, SCAN_START_OP.pose, self.time)
-            if self.reached_scan_start: self.time = 0
-        elif not self.scanning_done:
-            new_position, self.scanning_traj = self.pop(self.scanning_traj)
-            if new_position is None:
+        if not self.scanning_done:
+            if not self.scanning_traj:
                 self.scanning_done = True
             else:
-                new_pose = PoseStamped(pose=Pose(position=Point(x=new_position[0],y=new_position[1],z=new_position[2]),orientation=scan_orientation))
-                self.moveTo(new_pose)
+                self.moveTo(self.scanning_traj.pop(0))
         else:
             pass
+
 
     #-------------------------------------------------------------------------------------------------------------------
     #
@@ -103,24 +91,6 @@ class Robot(Node):
     def moveTo(self, target_pose):
         self.op_target_pub_.publish(target_pose)
 
-    def moveL(self, start_pose, target_pose, t):
-        if self.pose is None:
-            return False
-        start_pose = self.pose_to_array(start_pose)
-        target_pose = self.pose_to_array(target_pose)
-        # Move to a target pose by linearly interpolating from the current one.
-        command_position = (1 - t) * start_pose[:3] + t * target_pose[:3]
-        # SLERP on the quaternions
-        command_orientation = self.slerp(start_pose[3:], target_pose[3:], self.time)
-        # Send the pose command
-        command_pose = PoseStamped(
-            pose=Pose(position=Point(x=command_position[0],y=command_position[1],z=command_position[2]), 
-                      orientation=Quaternion(x=command_orientation[0],y=command_orientation[1],z=command_orientation[2],w=command_orientation[3])))
-        self.op_target_pub_.publish(command_pose)
-        # We did something, update the time
-        self.time += self.dt
-        return np.allclose(self.pose_to_array(self.pose), target_pose, rtol=0.01)
-
     #-------------------------------------------------------------------------------------------------------------------
     #
     #   High level motions
@@ -128,31 +98,43 @@ class Robot(Node):
     #-------------------------------------------------------------------------------------------------------------------
 
     def generate_scanning_task(self, 
-                               initial_pose = SCAN_START_OP.pose,
-                               center = np.array([0,0, 0.422]),
                                displacement = 0.2,
-                               rectilinear_dt = 0.5,
-                               circular_dt = 1.5):
-        starting_point = np.array([initial_pose.position.x, initial_pose.position.y, initial_pose.position.z])
+                               rectilinear_dt = 1.5,
+                               circular_dt = 2):
+        center = np.array([0.0, 0.0, self.SCAN_START_OP.pose.position.z])
+        # Linear motion to the scanning start
+        t, u = self.planner.linear_polynomial(0, rectilinear_dt, 0, 1)
+        q = self.planner.rectilinear_motion_primitive(u, self.HOME_OP, self.SCAN_START_OP)
         # First arc
-        t1, u1 = self.planner.linear_polynomial(0, circular_dt, -np.pi/2, np.pi/2)
-        q1 = self.planner.circular_motion_primitive(u1, starting_point, center)
+        t1, u1 = self.planner.linear_polynomial(t[-1], t[-1] + circular_dt, -np.pi/2, np.pi/2)
+        q1, d1 = self.planner.circular_motion_primitive(u1, q[-1], center)
         # First line
-        displacement_vector = displacement * (q1[-1,:] - center) / np.linalg.norm(q1[-1,:] - center)
+        displacement_vector = displacement * d1
+        q2f = copy.deepcopy(q1[-1])
+        q2f.pose.position.x += displacement_vector[0]
+        q2f.pose.position.y += displacement_vector[1]
+        q2f.pose.position.z += displacement_vector[2]
         t2, u2 = self.planner.linear_polynomial(t1[-1], t1[-1] + rectilinear_dt, 0, 1)
-        q2 = self.planner.rectilinear_motion_primitive(u2, q1[-1,:], q1[-1,:] + displacement_vector)
+        q2 = self.planner.rectilinear_motion_primitive(u2, q1[-1], q2f)
         # Second arc
         t3, u3 = self.planner.linear_polynomial(t2[-1], t2[-1] + circular_dt, 3*np.pi/2, np.pi/2)
-        q3 = self.planner.circular_motion_primitive(u3, q2[-1,:], center)
+        q3, d3 = self.planner.circular_motion_primitive(u3, q2[-1], center)
         # Second line
-        displacement_vector = displacement * (q3[-1,:] - center) / np.linalg.norm(q3[-1,:] - center)
+        displacement_vector = displacement * d3
+        q4f = copy.deepcopy(q3[-1])
+        q4f.pose.position.x += displacement_vector[0]
+        q4f.pose.position.y += displacement_vector[1]
+        q4f.pose.position.z += displacement_vector[2]
         t4, u4 = self.planner.linear_polynomial(t3[-1], t3[-1] + rectilinear_dt, 0, 1)
-        q4 = self.planner.rectilinear_motion_primitive(u4, q3[-1,:], q3[-1,:] + displacement_vector)
+        q4 = self.planner.rectilinear_motion_primitive(u4, q3[-1], q4f)
         # Final arc
-        _, u5 = self.planner.linear_polynomial(t4[-1], t4[-1] + circular_dt, -np.pi/2, np.pi/2)
-        q5 = self.planner.circular_motion_primitive(u5, q4[-1,:], center)
+        t5, u5 = self.planner.linear_polynomial(t4[-1], t4[-1] + circular_dt, -np.pi/2, np.pi/2)
+        q5, _ = self.planner.circular_motion_primitive(u5, q4[-1], center)
+        # Homing
+        _, u6 = self.planner.linear_polynomial(t5[-1], t5[-1] + rectilinear_dt, 0, 1)
+        q6 = self.planner.rectilinear_motion_primitive(u6, q5[-1], self.HOME_OP)
 
-        return np.concatenate((q1,q2,q3,q4,q5))
+        return q + q1 + q2 + q3 + q4 + q5 + q6
 
     #-------------------------------------------------------------------------------------------------------------------
     #
@@ -187,10 +169,7 @@ class Robot(Node):
                 cube_dict[cube] = data.pose
                 position = f"Position : ({data.pose.position.x:0.3f},{data.pose.position.y:0.3f},{data.pose.position.z:0.3f})"
                 orientation = f"Orientation : ({data.pose.orientation.x:0.3f},{data.pose.orientation.y:0.3f},{data.pose.orientation.z:0.3f},{data.pose.orientation.w:0.3f})"
-                self.log(f'Found {CUBE_NAMES[cube].lower()} cube {dest}: {position} {orientation}')
-                if not self.any_is_none(self.cube_locations) and not self.any_is_none(self.cube_destinations):
-                    # All entries were filled in, change the state
-                    self.scanning_done = True
+                self.log(f'Found {self.CUBE_NAMES[cube].lower()} cube {dest}: {position} {orientation}')
         return callback
     
     #-------------------------------------------------------------------------------------------------------------------
@@ -207,42 +186,3 @@ class Robot(Node):
             if val is None:
                 return True
         return False
-
-    def pose_to_array(self, pose : Pose):
-        return np.array([pose.position.x, 
-                        pose.position.y, 
-                        pose.position.z, 
-                        pose.orientation.x, 
-                        pose.orientation.y, 
-                        pose.orientation.z, 
-                        pose.orientation.w]) if pose is not None else None
-    
-    def normalize(self, array):
-        return array / np.linalg.norm(array)
-    
-    def slerp(self, q1, q2, t):
-        # Compute spherical linear interpolation between quaternions
-        # Ensure both input quaternions are normalized
-        q1 = self.normalize(q1)
-        q2 = self.normalize(q2)
-
-        dot_product = np.dot(q1, q2)
-
-        # Clamp the dot product to ensure it stays within [-1, 1] to avoid NaN in arccos
-        dot_product = np.clip(dot_product, -1.0, 1.0)
-
-        # Calculate the angle between the quaternions
-        theta = np.arccos(dot_product)
-
-        # Interpolate using spherical linear interpolation
-        sin_theta = np.sin(theta)
-        q_interpolated = (np.sin((1 - t) * theta) / sin_theta) * q1 + (np.sin(t * theta) / sin_theta) * q2
-
-        return self.normalize(q_interpolated)
-    
-    def pop(self, arr):
-        if arr is None:
-            return None, None
-        if arr.shape[0] == 1:
-            return arr[0,:], None
-        return arr[0,:], arr[1:,:]
--- Robotics/assignments/assignment7/assignment7/trajectory.py
+++ Robotics/assignments/assignment7/assignment7/trajectory.py
@@ -1,7 +1,7 @@
+import copy
 import numpy as np
 
-HOME_OP = np.array([0.484, 0.128, 0.609, 0.5, 0.5, 0.5, 0.5])
-SCAN_START_OP = np.array([-0.129, 0.487, 0.422, 0.0017937315462992396, 0.9999976506446824, 0.00035651772527578343, -0.001163669784592465])
+from geometry_msgs.msg import Point, Quaternion, Pose, PoseStamped
 
 class Trajectory():
     def __init__(self, dt=1/100) -> None:
@@ -21,57 +21,193 @@ class Trajectory():
         
         return t, q
     
-    def rectilinear_motion_primitive(self, u, qi, qf):
+    def rectilinear_motion_primitive(self, u, qi: PoseStamped, qf: PoseStamped):
+        # Extract the position and orientation  
+        pos_i = np.array([qi.pose.position.x,qi.pose.position.y,qi.pose.position.z])
+        rot_i = np.array([qi.pose.orientation.x,qi.pose.orientation.y,qi.pose.orientation.z,qi.pose.orientation.w])
+        pos_f = np.array([qf.pose.position.x,qf.pose.position.y,qf.pose.position.z])
+        rot_f = np.array([qf.pose.orientation.x,qf.pose.orientation.y,qf.pose.orientation.z,qf.pose.orientation.w])
         # Generate a rectilinear motion primitive between qi and qf, parametrized by u
-        dq = qf - qi
-        q = qi + u[:, np.newaxis]*dq
-        
+        dq = pos_f - pos_i
+        q = []
+        for t in u:
+            x, y, z = pos_i + t*dq
+            qx, qy, qz, qw = self.slerp(rot_i, rot_f, t)
+            q.append(PoseStamped(pose=Pose(position=Point(x=x,y=y,z=z),orientation=Quaternion(x=qx,y=qy,z=qz,w=qw))))
         return q
     
-    def circular_motion_primitive(self, u, P, C):
-        # Generate the circular motion primitive defined by a starting point P, center c, parametrized by u
-        direction_vector = P - C
+    def circular_motion_primitive(self, u, P: PoseStamped, C):
+        # Extract the position and orientation  
+        pos = np.array([P.pose.position.x,P.pose.position.y,P.pose.position.z])
+        rot = np.array([P.pose.orientation.x,P.pose.orientation.y,P.pose.orientation.z,P.pose.orientation.w])
+        rot_as_matrix = self.quaternion_to_rotation_matrix(rot)
+        z_axis_init = rot_as_matrix[:,-1]
+        # Compute the radius versor 
+        direction_vector = pos - C
         radius = np.linalg.norm(direction_vector)
         direction_vector = direction_vector / radius
-        q = np.zeros((u.shape[0], P.shape[0]))
-        for i, theta in enumerate(u):
-            q[i,:] = C + radius * (np.cos(theta + np.pi/2) * direction_vector + np.sin(theta + np.pi/2) * np.cross(direction_vector, [0,0,1]))
-        return q
+        # Generate the profile
+        q = []
+        for theta in u:
+            x, y, z = C + radius * (-np.sin(theta) * direction_vector + np.cos(theta) * np.cross(direction_vector, [0,0,1]))
+            
+            y_axis = -np.sin(theta) * direction_vector + np.cos(theta) * np.cross(direction_vector, [0,0,1])
+            y_axis = self.normalize(y_axis)
+            z_axis = z_axis_init
+            x_axis = np.cross(y_axis, z_axis)
+            x_axis = self.normalize(x_axis)
+            qx, qy, qz, qw = self.rotation_matrix_to_quaternion(np.array([x_axis, y_axis, z_axis]))
+            q.append(PoseStamped(pose=Pose(position=Point(x=x,y=y,z=z),orientation=Quaternion(x=qx,y=qy,z=qz,w=qw))))
+        # Compute the perpendicular vector to the last pose (useful to add rectilinear motion afterward)
+        d = np.array([q[-1].pose.position.x,q[-1].pose.position.y,q[-1].pose.position.z]) - C
+        return q, d
     
+    #-------------------------------------------------------------------------------------------------------------------
+    #
+    #   Utility functions
+    #
+    #-------------------------------------------------------------------------------------------------------------------
+
+    def slerp(self, q1, q2, t):
+        # Compute spherical linear interpolation between quaternions
+        q1 = self.normalize(q1)
+        q2 = self.normalize(q2)
+        dot_product = np.dot(q1, q2)
+        if dot_product < 0.0:
+            # Switching the sign ensures we take the shortest path on the quaternion sphere
+            q1 = -q1
+            dot_product = -dot_product
+        if dot_product > 0.95:
+            # They are basically the same quaternion
+            return q1
+        theta = np.arccos(dot_product)
+        q_interpolated = (np.sin((1 - t) * theta) / np.sin(theta)) * q1 + (np.sin(t * theta) / np.sin(theta)) * q2
+
+        return self.normalize(q_interpolated)
+    
+    def normalize(self, array):
+        return array / np.linalg.norm(array)
+    
+    def rotation_matrix_to_quaternion(self, matrix):
+        m00, m01, m02 = matrix[0, 0], matrix[0, 1], matrix[0, 2]
+        m10, m11, m12 = matrix[1, 0], matrix[1, 1], matrix[1, 2]
+        m20, m21, m22 = matrix[2, 0], matrix[2, 1], matrix[2, 2]
+        trace = m00 + m11 + m22
+        if trace > 0:
+            s = 0.5 / np.sqrt(trace + 1.0)
+            x = (m21 - m12) * s
+            y = (m02 - m20) * s
+            z = (m10 - m01) * s
+            w = 0.25 / s
+        else:
+            if m00 > m11 and m00 > m22:
+                s = 0.5 / np.sqrt(1.0 + m00 - m11 - m22)
+                x = 0.25 / s
+                y = (m01 + m10) * s
+                z = (m02 + m20) * s
+                w = (m21 - m12) * s
+            elif m11 > m22:
+                s = 0.5 / np.sqrt(1.0 + m11 - m00 - m22)
+                x = (m01 + m10) * s
+                y = 0.25 / s
+                z = (m12 + m21) * s
+                w = (m02 - m20) * s
+            else:
+                s = 0.5 / np.sqrt(1.0 + m22 - m00 - m11)
+                x = (m02 + m20) * s
+                y = (m12 + m21) * s
+                z = 0.25 / s
+                w = (m10 - m01) * s
+        return x, y, z, w
+    
+    def quaternion_to_rotation_matrix(self, quaternion):
+        quaternion = self.normalize(quaternion)
+        x, y, z, w = quaternion
+        xx = x * x
+        xy = x * y
+        xz = x * z
+        yy = y * y
+        yz = y * z
+        zz = z * z
+        wx = w * x
+        wy = w * y
+        wz = w * z
+        matrix = np.array([
+            [1 - 2 * (yy + zz), 2 * (xy - wz), 2 * (xz + wy)],
+            [2 * (xy + wz), 1 - 2 * (xx + zz), 2 * (yz - wx)],
+            [2 * (xz - wy), 2 * (yz + wx), 1 - 2 * (xx + yy)]
+        ])
+        return matrix
+
 if __name__ == '__main__':
-    traj = Trajectory()
+    planner = Trajectory()
     # Scanning trajectory parameters
     starting_point = np.array([0.484, 0.128, 0.609]) # Trajectory start
     center = np.array([0,0.128,0]) # Center for circular primitives
     displacement = 0.2 # Distance to cover during rectilinear motion
-    rectlinear_dt = 0.5 # Time duration for rectilinear motion
+    rectilinear_dt = 1.5 # Time duration for rectilinear motion
     circular_dt = 1.5 # Time for circular motion
+    
+    scan_orientation = Quaternion(x=0.0017937315462992396, y=0.9999976506446824, z=0.00035651772527578343, w=-0.001163669784592465)
+    initial_pose = PoseStamped(pose=Pose(position=Point(x=0.484, y=0.128, z=0.609), orientation=Quaternion(x=0.5, y=0.5, z=0.5, w=0.5)))
+    scanning_start = PoseStamped(pose=Pose(position=Point(x=-0.129, y=0.487, z=0.422), 
+                                        orientation=scan_orientation))
+
+    center = np.array([0.0, 0.0, scanning_start.pose.position.z])
+    # Linear motion to the scanning start
+    t, u = planner.linear_polynomial(0, rectilinear_dt, 0, 1)
+    q = planner.rectilinear_motion_primitive(u, initial_pose, scanning_start)
     # First arc
-    t1, u1 = traj.linear_polynomial(0, circular_dt, -np.pi/2, np.pi/2)
-    q1 = traj.circular_motion_primitive(u1, starting_point, center)
+    t1, u1 = planner.linear_polynomial(t[-1], t[-1] + circular_dt, -np.pi/2, np.pi/2)
+    q1, d1 = planner.circular_motion_primitive(u1, q[-1], center)
     # First line
-    displacement_vector = displacement * (q1[-1,:] - center) / np.linalg.norm(q1[-1,:] - center)
-    t2, u2 = traj.linear_polynomial(t1[-1], t1[-1] + rectlinear_dt, 0, 1)
-    q2 = traj.rectilinear_motion_primitive(u2, q1[-1,:], q1[-1,:] + displacement_vector)
+    displacement_vector = displacement * d1
+    q2f = copy.deepcopy(q1[-1])
+    q2f.pose.position.x += displacement_vector[0]
+    q2f.pose.position.y += displacement_vector[1]
+    q2f.pose.position.z += displacement_vector[2]
+    t2, u2 = planner.linear_polynomial(t1[-1], t1[-1] + rectilinear_dt, 0, 1)
+    q2 = planner.rectilinear_motion_primitive(u2, q1[-1], q2f)
     # Second arc
-    t3, u3 = traj.linear_polynomial(t2[-1], t2[-1] + circular_dt, 3*np.pi/2, np.pi/2)
-    q3 = traj.circular_motion_primitive(u3, q2[-1,:], center)
+    t3, u3 = planner.linear_polynomial(t2[-1], t2[-1] + circular_dt, 3*np.pi/2, np.pi/2)
+    q3, d3 = planner.circular_motion_primitive(u3, q2[-1], center)
     # Second line
-    displacement_vector = displacement * (q3[-1,:] - center) / np.linalg.norm(q3[-1,:] - center)
-    t4, u4 = traj.linear_polynomial(t3[-1], t3[-1] + rectlinear_dt, 0, 1)
-    q4 = traj.rectilinear_motion_primitive(u4, q3[-1,:], q3[-1,:] + displacement_vector)
+    displacement_vector = displacement * d3
+    q4f = copy.deepcopy(q3[-1])
+    q4f.pose.position.x += displacement_vector[0]
+    q4f.pose.position.y += displacement_vector[1]
+    q4f.pose.position.z += displacement_vector[2]
+    t4, u4 = planner.linear_polynomial(t3[-1], t3[-1] + rectilinear_dt, 0, 1)
+    q4 = planner.rectilinear_motion_primitive(u4, q3[-1], q4f)
     # Final arc
-    _, u5 = traj.linear_polynomial(t4[-1], t4[-1] + circular_dt, -np.pi/2, np.pi/2)
-    q5 = traj.circular_motion_primitive(u5, q4[-1,:], center)
-
-    q = np.concatenate((q1,q2,q3,q4,q5))
-
+    _, u5 = planner.linear_polynomial(t4[-1], t4[-1] + circular_dt, -np.pi/2, np.pi/2)
+    q5, _ = planner.circular_motion_primitive(u5, q4[-1], center)
+    q = q + q1 + q2 + q3 + q4 + q5
+    for i, posestamped in enumerate(q):
+        pose = posestamped.pose
+        q[i] = np.array([pose.position.x,pose.position.y,pose.position.z,pose.orientation.x,pose.orientation.y,pose.orientation.z,pose.orientation.w])
+    q = np.vstack((q))
     import matplotlib.pyplot as plt
     from mpl_toolkits.mplot3d import Axes3D
     plt.figure()
     ax = plt.subplot(projection='3d')
     ax.plot(q[:,0],q[:,1],q[:,2])
+    for i in range(q.shape[0]):
+        if i % 10 == 0:
+            x, y, z = q[i,:3]
+            qx, qy, qz, qw = q[i,3:]
+            u = np.array([1 - 2*(qy**2 + qz**2), 2*(qx*qy + qz*qw), 2*(qx*qz - qy*qw)])
+            u /= np.linalg.norm(u)
+            v = np.array([2*(qx*qy - qz*qw), 1 - 2*(qx**2 + qz**2), 2*(qy*qz + qx*qw)])
+            v /= np.linalg.norm(v)
+            w = np.array([2*(qx*qz + qy*qw), 2*(qy*qz - qx*qw), 1 - 2*(qx**2 + qy**2)])
+            w /= np.linalg.norm(w)
+            ax.quiver(x, y, z, u[0], u[1], u[2], length=0.05, normalize=True, color='red')
+            ax.quiver(x, y, z, v[0], v[1], v[2], length=0.05, normalize=True, color='green')
+            ax.quiver(x, y, z, w[0], w[1], w[2], length=0.025, normalize=True, color='blue')
+        
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')
+    ax.view_init(azim=60, elev=45)
     plt.show()
