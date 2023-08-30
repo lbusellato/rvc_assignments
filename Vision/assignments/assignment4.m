98e32d0f2ac5ff7a2e904c2420bb936e6a9dbfd7
lbusellato <lorenzo.busellato@gmail.com>
2023-08-30 13:20:00 +0200

    Vision assignment 4 - added original example

--- Vision/assignments/assignment4.m
+++ Vision/assignments/assignment4.m
@@ -16,7 +16,8 @@ addpath(genpath('../data/assignment4/'));
 
 %% LOAD THE IMAGE AND CONVERT TO B/W
 
-I = rgb2gray(imread('minuteria2.jpeg'));
+imageNo = 1;
+I = rgb2gray(imread(strcat(num2str(imageNo),'.png')));
 figure(1);
 imshow(I); title('Original image'); drawnow;
 
@@ -42,7 +43,7 @@ imshow(bw); title("Extracted shapes"); drawnow;
 
 cc = bwconncomp(bw,4);
 stats = regionprops('table',bw,'Centroid','Circularity','EquivDiameter','Image','Orientation','MajorAxisLength','MinorAxisLength');
-
+% Properties needed for shape classification
 circularities = stats.Circularity;
 centroids = stats.Centroid;
 diameters = stats.EquivDiameter;
@@ -50,32 +51,59 @@ images = stats.Image;
 orientations = stats.Orientation;
 majorAxes = stats.MajorAxisLength;
 minorAxes = stats.MinorAxisLength;
-
+% Shape classification
 figure(3); imshow(I); hold on;
-for i=1:size(circularities,1)
-    if circularities(i) > 0.9
-        % Washer detected, estimate the inner diameter (all numbers are empiric)
-        x = (majorAxes(i) + minorAxes(i))/2;
-        diameter = round((3.21821146612328e-07)*x.^5 + (-0.000132741145229905)*x.^4 + 0.0212554536472068*x.^3 + (-1.64860773332881)*x.^2 + 61.920797953864*x + (-895.382337959459));
-        % Based on the diameter, get the denomination
-        denomination = "Washer M" + num2str(diameter);
-        text(centroids(i,1),centroids(i,2),denomination,'Color','red');
-    else
-        % Bolt detected, extract it as a new image and isolate the thread part
-        img = images{i};
-        new_angle = sign(orientations(i))*90 - orientations(i);
-        img = imrotate(img, new_angle);
-        img = img(int32(size(img,1)/2):size(img,1),:);
-        % Based on the lenghts of the axes, get the denomination
-        stats = regionprops('table',img,'MajorAxisLength','MinorAxisLength');
-        % Bolt length related to the major axis
-        x = stats.MajorAxisLength;
-        length = round(0.00020929*x^3 - 0.0511*x^2 + 4.1439*x - 91.7938);
-        % Bolt diameter related to the minor axis
-        x = stats.MinorAxisLength;
-        diameter = round(0.0330*x^3 - 2.1381*x^2 + 46.1296*x - 325.5931);
-        denomination = "Bolt M" + num2str(diameter) + "x" + num2str(length);
-        text(centroids(i,1),centroids(i,2),denomination,'Color','red');
+if imageNo == 1
+    % Coins and USB
+    for i=1:size(circularities,1)
+        if circularities(i) > 0.9
+            % Coin detected
+            radius = (majorAxes(i) + minorAxes(i)) / 2;
+            % Is it the big coin?
+            bigCoin = false;
+            for j=i+1:size(circularities,1)
+                if circularities(j) > 0.9
+                    radius2 = (majorAxes(j) + minorAxes(j)) / 2;
+                    bigCoin = radius > radius2;
+                end
+            end
+            if bigCoin
+                text(centroids(i,1),centroids(i,2),"Big coin",'Color','red');
+            else
+                text(centroids(i,1),centroids(i,2),"Small coin",'Color','red');
+            end
+        else
+            % USB detected
+            text(centroids(i,1),centroids(i,2),"USB stick",'Color','red');
+        end
+    end
+else
+    % Washers and bolts
+    for i=1:size(circularities,1)
+        if circularities(i) > 0.9
+            % Washer detected, estimate the inner diameter (all numbers are empiric)
+            x = (majorAxes(i) + minorAxes(i))/2;
+            diameter = round((3.21821146612328e-07)*x.^5 + (-0.000132741145229905)*x.^4 + 0.0212554536472068*x.^3 + (-1.64860773332881)*x.^2 + 61.920797953864*x + (-895.382337959459));
+            % Based on the diameter, get the denomination
+            denomination = "Washer M" + num2str(diameter);
+            text(centroids(i,1),centroids(i,2),denomination,'Color','red');
+        else
+            % Bolt detected, extract it as a new image and isolate the thread part
+            img = images{i};
+            new_angle = sign(orientations(i))*90 - orientations(i);
+            img = imrotate(img, new_angle);
+            img = img(int32(size(img,1)/2):size(img,1),:);
+            % Based on the lenghts of the axes, get the denomination
+            stats = regionprops('table',img,'MajorAxisLength','MinorAxisLength');
+            % Bolt length related to the major axis
+            x = stats.MajorAxisLength;
+            length = round(0.00020929*x^3 - 0.0511*x^2 + 4.1439*x - 91.7938);
+            % Bolt diameter related to the minor axis
+            x = stats.MinorAxisLength;
+            diameter = round(0.0330*x^3 - 2.1381*x^2 + 46.1296*x - 325.5931);
+            denomination = "Bolt M" + num2str(diameter) + "x" + num2str(length);
+            text(centroids(i,1),centroids(i,2),denomination,'Color','red');
+        end
     end
 end
 title("Labeled image"); drawnow;
